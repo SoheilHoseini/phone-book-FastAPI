209440fd8be81eed32359a95f060cdd4fb0c8ef3
Soheil Hoseini <soheil.hosseini78@gmail.com>
2023-07-03 20:06:27 +0330

    implement a sample Create operation and a sample database model

--- phonebook-project/main.py
+++ phonebook-project/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI, status
-from database import Base, engine
+from database import Base, engine, ToDo
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+
+# Create ToDoRequest Base Model => I think it determines how a new record should be created
+class ToDoRequest(BaseModel):
+    task: str
+
 
 # Create the database
 Base.metadata.create_all(engine)
@@ -12,8 +20,25 @@ def root():
     return "todooo"
 
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
-def create_todo():
-    return "create todo item"
+def create_todo(todo: ToDoRequest):
+    # create a new database session
+    session = Session(bind=engine, expire_on_commit=False)
+
+    # create an instance of the ToDo database model
+    tododb = ToDo(task = todo.task)
+
+    # add it to the session and commit it
+    session.add(tododb)
+    session.commit()
+
+    # grab the id given to the object from the database
+    id = tododb.id
+
+    # close the session
+    session.close()
+
+    # return the id
+    return f"created todo item with id {id}"
 
 @app.get("/todo/{id}")
 def read_todo(id: int):
