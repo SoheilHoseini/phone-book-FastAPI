37e6869d0193de6c7dd68799f6948e35dc7a6324
Soheil Hoseini <soheil.hosseini78@gmail.com>
2023-07-05 20:49:22 +0330

    refactor database session usage

--- phonebook-project/main.py
+++ phonebook-project/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, status, HTTPException
+from typing import List
+from fastapi import FastAPI, status, HTTPException, Depends
 from database import Base, engine, SessionLocal
+from sqlalchemy.orm import Session
 import models
 import schemas
 
@@ -9,15 +11,23 @@ Base.metadata.create_all(engine)
 # Initialize app
 app = FastAPI()
 
+
+# Helper function to get database session
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
+
+
 @app.get("/")
 def root():
     return "todooo"
 
-@app.post("/todo", status_code=status.HTTP_201_CREATED)
-def create_todo(todo: schemas.ToDo):
 
-    # create a new database session
-    session = SessionLocal()
+@app.post("/todo", response_model=schemas.ToDo, status_code=status.HTTP_201_CREATED)
+def create_todo(todo: schemas.ToDoCreate, session: Session = Depends(get_session)):
 
     # create an instance of the ToDo database model
     tododb = models.ToDo(task = todo.task)
@@ -25,27 +35,17 @@ def create_todo(todo: schemas.ToDo):
     # add it to the session and commit it
     session.add(tododb)
     session.commit()
+    session.refresh(tododb)
 
-    # grab the id given to the object from the database
-    id = tododb.id
+    # return the todo object
+    return tododb
 
-    # close the session
-    session.close()
 
-    # return the id
-    return f"created todo item with id {id}"
-
-@app.get("/todo/{id}")
-def read_todo(id: int):
-
-    # create a new database session
-    session = SessionLocal()
+@app.get("/todo/{id}", response_model=schemas.ToDo)
+def read_todo(id: int, session: Session = Depends(get_session)):
 
     # get the todo item with the given id
     todo = session.query(models.ToDo).get(id)
-
-    # close the session
-    session.close()
 
     # check if todo item with given id exists. If not, raise exception and return 404 not found response
     if not todo:
@@ -53,11 +53,9 @@ def read_todo(id: int):
 
     return todo
 
-@app.put("/todo/{id}")
-def update_todo(id: int, task: str):
 
-    # create a new database session
-    session = SessionLocal()
+@app.put("/todo/{id}", response_model=schemas.ToDo)
+def update_todo(id: int, task: str, session: Session = Depends(get_session)):
 
     # get the todo item with the given id
     todo = session.query(models.ToDo).get(id)
@@ -67,20 +65,15 @@ def update_todo(id: int, task: str):
         todo.task = task
         session.commit()
 
-    # close the session
-    session.close()
-
     # check if todo item with given id exists. If not, raise exception and return 404 not found response
     if not todo:
         raise HTTPException(status_code=404, detail=f"todo item with id {id} not found")
 
     return todo
 
-@app.delete("/todo/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_todo(id: int):
 
-    # create a new database session
-    session = SessionLocal()
+@app.delete("/todo/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_todo(id: int, session: Session = Depends(get_session)):
 
     # get the todo item with the given id
     todo = session.query(models.ToDo).get(id)
@@ -89,21 +82,16 @@ def delete_todo(id: int):
     if todo:
         session.delete(todo)
         session.commit()
-        session.close()
     else:
         raise HTTPException(status_code=404, detail=f"todo item with id {id} not found")
 
-    return f"todo item with id {id} deleted!"
+    return f"todo item with id {id} deleted"
 
-@app.get("/todo")
-def read_todo_list():
-    # create a new database session
-    session = SessionLocal()
+
+@app.get("/todo", response_model = List[schemas.ToDo])
+def read_todo_list(session: Session = Depends(get_session)):
 
     # get all todo items
     todo_list = session.query(models.ToDo).all()
-
-    # close the session
-    session.close()
 
     return todo_list
--- phonebook-project/schemas.py
+++ phonebook-project/schemas.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 # Create ToDo Schema (Pydantic Model)
 class ToDoCreate(BaseModel):
     task: str
-    
+
 # Complete ToDo Schema (Pydantic Model)
 class ToDo(BaseModel):
     id: int
